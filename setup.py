e0c6751cbcc3aeefd61cf99db9dc8104a518360b
davisagli <davisagli@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-06 01:25:35 +0000

    fix dependency, prep for release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.0.1'
 
 setup(name='collective.recaptcha',
       version=version,
@@ -25,7 +25,7 @@ setup(name='collective.recaptcha',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'recaptcha-client',
+          'recaptcha-client != 1.0.4',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
