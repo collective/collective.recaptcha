5557101cb090181f18c104e2fce719e0311a0422
Alex Clark <aclark@aclark.net>
2012-10-29 14:35:52 -0400

    Add bbb.py

---
+++ collective/recaptcha/bbb.py
@@ -0,0 +1,13 @@
+# BBB
+try:
+    from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
+except:
+    from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+try:
+    from zope.component.hooks import getSite
+except:
+    from zope.app.component.hooks import getSite
+try:
+    from zope.browser.interfaces import IAdding
+except:
+    from zope.app.container.interfaces import IAdding
