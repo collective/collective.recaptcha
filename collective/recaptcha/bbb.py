b8cb57c0e15a2dd7ce90bedbfc84ec96ca937895
Alex Clark <aclark@aclark.net>
2012-10-30 08:30:08 -0400

    Add BBB for ISite and import ISite from bbb.py

--- collective/recaptcha/bbb.py
+++ collective/recaptcha/bbb.py
@@ -11,3 +11,7 @@ try:
     from zope.browser.interfaces import IAdding
 except:
     from zope.app.container.interfaces import IAdding
+try:
+    from zope.component.interfaces import ISite
+except:
+    from zope.app.component.interfaces import ISite
