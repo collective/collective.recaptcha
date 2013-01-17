b81f63e47073f4f7fc2210df05cdf4a8aebd7de0
JeanMichel FRANCOIS <toutpt@gmail.com>
2013-01-17 11:17:51 +0100

    move doc to RST

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1.4dev'
 setup(name='collective.recaptcha',
       version=version,
       description="Wraps the recaptcha-client library to provide a drop-in replacement for collective.captcha.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
