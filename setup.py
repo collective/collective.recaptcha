6f2ad933be29c84d60b2fb31ff72a68abb9d205d
Martin Peeters <martin.peeters@affinitic.be>
2014-05-07 16:14:43 +0200

    Pep8 fix

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ version = '1.1.5'
 setup(name='collective.recaptcha',
       version=version,
       description="Wraps the recaptcha-client library to provide a drop-in replacement for collective.captcha.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=(open("README.rst").read() + "\n" +
+                        open(os.path.join("docs", "HISTORY.txt")).read()),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Zope2",
-        ],
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Framework :: Zope2",
+      ],
       keywords='captcha recaptcha zope plone',
       author='David Glick, Groundwire',
       author_email='davidglick@groundwire.org',
@@ -32,7 +32,7 @@ setup(name='collective.recaptcha',
       extras_require={
           'test': [
               'plone.app.testing',
-      ]},
+          ]},
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
