0860a09b6265a05b209db2c4f48dea23771a25bf
wesleybl <wesleybl@gmail.com>
2021-12-07 14:57:53 -0300

    lint

--- src/collective/recaptcha/bbb.py
+++ src/collective/recaptcha/bbb.py
@@ -2,17 +2,9 @@
 
 # BBB
 try:
-    from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
-except ImportError:
-    from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile  # noqa: F401 E501
-try:
     from zope.component.hooks import getSite
 except ImportError:
     from zope.app.component.hooks import getSite  # noqa: F401
-try:
-    from zope.browser.interfaces import IAdding
-except ImportError:
-    from zope.app.container.interfaces import IAdding  # noqa: F401
 try:
     from zope.component.interfaces import ISite
 except ImportError:
